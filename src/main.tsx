95722237fd128c3bb444f8fed1dc7911c2c4f17e
emerss001 <emersonnevess80@gmail.com>
2024-10-03 21:06:20 -0300

    criando página para agendar consulta

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./pages/home/Home.tsx";
 import "./index.css";
+import AgendarConsulta from "./pages/consulta/agendar-consulta.tsx";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <App />
+        <AgendarConsulta />
     </StrictMode>
 );
---
+++ src/pages/consulta/agendar-consulta.tsx
@@ -0,0 +1,30 @@
+import Navbar from "../../ui/navbar";
+
+const AgendarConsulta = () => {
+    return (
+        <>
+            <Navbar />
+            <div className="text-center max-w-[600px] mx-auto mt-14 mb-20">
+                <h1 className="font-bold text-5xl">Preencha o formulário para agendar a consulta</h1>
+            </div>
+
+            <div>
+                <form action="">
+                    <div className="px-40">
+                        <label htmlFor="" className="font-semibold text-xl">
+                            Nome do Responsável: <span className="text-red-600">*</span>
+                        </label>
+                        <input
+                            type="text"
+                            name=""
+                            className="w-full h-14 rounded-2xl text-2xl px-4 placeholder:font-semibold placeholder:text-xl"
+                            placeholder="Jhon Caster"
+                        />
+                    </div>
+                </form>
+            </div>
+        </>
+    );
+};
+
+export default AgendarConsulta;
